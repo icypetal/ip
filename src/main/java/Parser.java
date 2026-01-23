c41f913986574739e29ed5393e1e9033bb4e29b4
ljxr1337 <65820514+ljxr1337@users.noreply.github.com>
2026-01-23 15:25:22 +0800

    Implement A-MoreOOP refactoring (Added .gitignore)

---
+++ src/main/java/Parser.java
@@ -0,0 +1,10 @@
+public class Parser {
+    public static Command parse(String fullCommand) throws AgyException {
+        String commandStr = fullCommand.split(" ")[0].toUpperCase();
+        try {
+            return Command.valueOf(commandStr);
+        } catch (IllegalArgumentException e) {
+            throw new AgyException("Error: Unknown command");
+        }
+    }
+}
