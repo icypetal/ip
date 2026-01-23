cb2625e80ab5c6e1f6d00282fe6019076d2367b1
ljxr1337 <65820514+ljxr1337@users.noreply.github.com>
2026-01-23 20:31:35 +0800

    A-JavaDoc: Add JavaDoc comments to key classes

--- src/main/java/agy/parser/Parser.java
+++ src/main/java/agy/parser/Parser.java
@@ -3,7 +3,17 @@ package agy.parser;
 import agy.command.Command;
 import agy.exception.AgyException;
 
+/**
+ * Parses user input commands.
+ */
 public class Parser {
+    /**
+     * Parses the full user command and returns the corresponding Command enum.
+     *
+     * @param fullCommand The full command string entered by the user.
+     * @return The Command enum corresponding to the user input.
+     * @throws AgyException If the command is unknown.
+     */
     public static Command parse(String fullCommand) throws AgyException {
         String commandStr = fullCommand.split(" ")[0].toUpperCase();
         try {
--- src/main/java/agy/task/TaskList.java
+++ src/main/java/agy/task/TaskList.java
@@ -3,33 +3,72 @@ package agy.task;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Represents a list of tasks.
+ * Provides methods to add, delete, and retrieve tasks.
+ */
 public class TaskList {
     private List<Task> tasks;
 
+    /**
+     * Initializes an empty TaskList.
+     */
     public TaskList() {
         this.tasks = new ArrayList<>();
     }
 
+    /**
+     * Initializes a TaskList with an existing list of tasks.
+     *
+     * @param tasks The list of tasks to initialize with.
+     */
     public TaskList(List<Task> tasks) {
         this.tasks = tasks;
     }
 
+    /**
+     * Adds a task to the list.
+     *
+     * @param task The task to add.
+     */
     public void add(Task task) {
         tasks.add(task);
     }
 
+    /**
+     * Deletes a task from the list at the specified index.
+     *
+     * @param index The index of the task to delete.
+     * @return The deleted task.
+     */
     public Task delete(int index) {
         return tasks.remove(index);
     }
 
+    /**
+     * Retrieves a task from the list at the specified index.
+     *
+     * @param index The index of the task to retrieve.
+     * @return The task at the specified index.
+     */
     public Task get(int index) {
         return tasks.get(index);
     }
 
+    /**
+     * Returns the number of tasks in the list.
+     *
+     * @return The size of the task list.
+     */
     public int size() {
         return tasks.size();
     }
 
+    /**
+     * Returns the list of all tasks.
+     *
+     * @return The list of tasks.
+     */
     public List<Task> getAll() {
         return tasks;
     }
